a338e7c500b4fd315e037d5f8f21b0de029bb831
ashiofumiirerujiyu <ashiofumiirerujiyu@gmail.com>
2024-09-27 03:49:13 +0000

    Refactor: show -> save

--- ch03/sig_step_compare.py
+++ ch03/sig_step_compare.py
@@ -8,7 +8,7 @@ def sigmoid(x):
 
 
 def step_function(x):
-    return np.array(x > 0, dtype=np.int)
+    return np.array(x > 0, dtype=np.int8)
 
 x = np.arange(-5.0, 5.0, 0.1)
 y1 = sigmoid(x)
@@ -17,4 +17,4 @@ y2 = step_function(x)
 plt.plot(x, y1)
 plt.plot(x, y2, 'k--')
 plt.ylim(-0.1, 1.1) # y축 범위 지정
-plt.show()
+plt.savefig('sig_step_fuction.jpg')
