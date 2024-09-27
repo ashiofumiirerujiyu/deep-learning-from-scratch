59979a351cb3ab4809c5a93076840874b252d893
ashiofumiirerujiyu <ashiofumiirerujiyu@gmail.com>
2024-09-27 03:49:22 +0000

    Add: Compare time check

--- ch03/neuralnet_mnist.py
+++ ch03/neuralnet_mnist.py
@@ -1,22 +1,20 @@
 # coding: utf-8
 import sys, os
+import time  # 시간 측정 추가
 sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import pickle
 from dataset.mnist import load_mnist
 from common.functions import sigmoid, softmax
 
-
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
-
 
 def init_network():
     with open("sample_weight.pkl", 'rb') as f:
         network = pickle.load(f)
     return network
-
 
 def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
@@ -31,14 +29,18 @@ def predict(network, x):
 
     return y
 
-
 x, t = get_data()
 network = init_network()
+
 accuracy_cnt = 0
+start_time = time.time()  # 시간 측정 시작
+
 for i in range(len(x)):
     y = predict(network, x[i])
-    p= np.argmax(y) # 확률이 가장 높은 원소의 인덱스를 얻는다.
+    p = np.argmax(y)  # 확률이 가장 높은 원소의 인덱스를 얻는다.
     if p == t[i]:
         accuracy_cnt += 1
 
-print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+end_time = time.time()  # 시간 측정 종료
+print("Accuracy: " + str(float(accuracy_cnt) / len(x)))
+print("Execution time: " + str(end_time - start_time) + " seconds")
--- ch03/neuralnet_mnist_batch.py
+++ ch03/neuralnet_mnist_batch.py
@@ -1,47 +1,47 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 
+import time  # 시간 측정 추가
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
 import pickle
 from dataset.mnist import load_mnist
 from common.functions import sigmoid, softmax
 
-
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
-
 
 def init_network():
     with open("sample_weight.pkl", 'rb') as f:
         network = pickle.load(f)
     return network
 
-
 def predict(network, x):
-    w1, w2, w3 = network['W1'], network['W2'], network['W3']
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-    a1 = np.dot(x, w1) + b1
+    a1 = np.dot(x, W1) + b1
     z1 = sigmoid(a1)
-    a2 = np.dot(z1, w2) + b2
+    a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
-    a3 = np.dot(z2, w3) + b3
+    a3 = np.dot(z2, W3) + b3
     y = softmax(a3)
 
     return y
 
-
 x, t = get_data()
 network = init_network()
 
-batch_size = 100 # 배치 크기
+batch_size = 100  # 배치 크기
 accuracy_cnt = 0
+start_time = time.time()  # 시간 측정 시작
 
 for i in range(0, len(x), batch_size):
-    x_batch = x[i:i+batch_size]
+    x_batch = x[i:i + batch_size]
     y_batch = predict(network, x_batch)
     p = np.argmax(y_batch, axis=1)
-    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+    accuracy_cnt += np.sum(p == t[i:i + batch_size])
 
-print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+end_time = time.time()  # 시간 측정 종료
+print("Accuracy: " + str(float(accuracy_cnt) / len(x)))
+print("Execution time: " + str(end_time - start_time) + " seconds")
